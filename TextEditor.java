b77a3b4efd005840e3f16b5c1972737a7e72d067
anhquyen18 <anhquyen18092000@gmail.com>
2021-07-12 15:35:03 +0700

    add initial interface

--- TextEditor.java
+++ TextEditor.java
@@ -1,4 +1,21 @@
+import javax.swing.*;
 
-public class TextEditor {
-
+import java.awt.*;
+import java.awt.event.*;
+public class TextEditor extends JFrame implements ActionListener{
+	TextEditor(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("AQ Text Editor");
+		this.setSize(500, 500);
+		this.setLayout(new FlowLayout());
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
